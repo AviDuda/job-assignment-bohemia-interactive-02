993ebd199466fef14dc7eace2e06a82c818bc6e2
Avi Duda <368677+AviDuda@users.noreply.github.com>
2022-07-04 11:40:07 +0200

    More responsive checkboxes

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -9,14 +9,16 @@ interface ErrorMessageProps {
 
 export default function Checkbox({ label, checked, onChange, labelClassName = "" }: ErrorMessageProps) {
     return (
-        <label className="mb-10 flex w-fit cursor-pointer items-center pl-1">
+        <label className="mb-10 flex max-w-[90%] cursor-pointer flex-wrap items-center gap-6 pl-1">
             <input
                 type="checkbox"
                 checked={checked}
                 onChange={onChange}
                 className="grid h-9 w-9 cursor-pointer appearance-none place-content-center border-2 border-black text-black focus:ring-2 focus:ring-current dark:border-zinc-800 dark:bg-zinc-700 dark:text-zinc-800 dark:checked:border-zinc-600 lg:h-6 lg:w-6"
             />
-            <span className={`select-none pl-6 text-3xl text-gray-900 dark:text-zinc-200 lg:text-xl ${labelClassName}`}>
+            <span
+                className={`flex-1 select-none text-3xl text-gray-900 dark:text-zinc-200 lg:text-xl ${labelClassName}`}
+            >
                 {label}
             </span>
         </label>
