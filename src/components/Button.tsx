1e5b8bf04464a448e479a5eb860b43e805052190
Avi Duda <368677+AviDuda@users.noreply.github.com>
2022-07-04 09:19:28 +0200

    Invertable button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,30 @@
+import clsx from "clsx";
 import { MouseEventHandler, ReactNode } from "react";
 
 interface ButtonProps {
     href: string;
+    invert?: boolean;
     className?: string;
     onClick?: MouseEventHandler<HTMLAnchorElement>;
     children?: ReactNode;
 }
 
-export default function Button({ href, onClick, className = "", children }: ButtonProps) {
+export default function Button({ href, onClick, invert = false, className = "", children }: ButtonProps) {
+    const defaultColors = {
+        normal: "bg-black text-white outline-black hover:bg-white hover:text-black hover:outline-black",
+        inverted: "bg-white text-black outline-black hover:bg-black hover:text-white",
+    };
+
     return (
         <a
             href={href}
             onClick={onClick}
-            className={`bg-black px-10 py-2 text-center text-2xl font-medium tracking-widest text-white hover:text-gray-300 ${className}`}
+            className={clsx(
+                `py-2 px-10 text-center text-2xl font-medium tracking-widest outline outline-2 outline-offset-[-2px]`,
+                !invert && defaultColors.normal,
+                invert && defaultColors.inverted,
+                className,
+            )}
         >
             {children}
         </a>
