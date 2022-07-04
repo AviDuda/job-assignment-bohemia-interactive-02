9e54208616b524189e6d0c741456d5d65c77fbf2
Avi Duda <368677+AviDuda@users.noreply.github.com>
2022-07-04 05:25:31 +0200

    Override select caret's color
    It can't be customized in `@tailwindcss/forms`.
    See https://github.com/tailwindlabs/tailwindcss-forms/issues/17

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
+const plugin = require("tailwindcss/plugin");
 const defaultTheme = require("tailwindcss/defaultTheme");
+const svgToDataUri = require("mini-svg-data-uri");
 
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig["theme"]} */
 module.exports = {
     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-    plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        plugin(function ({ addComponents, theme }) {
+            // Override select's caret color as it can't be customized in @tailwindcss/forms
+            // see https://github.com/tailwindlabs/tailwindcss-forms/issues/17
+            addComponents({
+                select: {
+                    "background-image": `url("${svgToDataUri(
+                        `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path stroke="${theme(
+                            "colors.black",
+                        )}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 8l4 4 4-4"/></svg>`,
+                    )}")`,
+                },
+            });
+        }),
+    ],
     theme: {
         extend: {
             container: {
