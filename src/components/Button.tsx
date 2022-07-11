5c104d686ea3959a2f7882ffc0fda73b6c597c18
Avi Duda <368677+AviDuda@users.noreply.github.com>
2022-07-11 18:24:38 +0200

    Move all button classes directly to className

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,20 +10,16 @@ interface ButtonProps {
 }
 
 export default function Button({ href, onClick, invert = false, className = "", children }: ButtonProps) {
-    const defaultColors = {
-        normal: "bg-black text-white outline-black hover:bg-white hover:text-black hover:outline-black dark:bg-white dark:text-black dark:outline-white dark:hover:bg-black dark:hover:text-white dark:hover:outline-black",
-        inverted:
-            "bg-white text-black outline-black hover:bg-black hover:text-white dark:bg-black dark:text-white dark:hover:bg-white dark:hover:text-black",
-    };
-
     return (
         <a
             href={href}
             onClick={onClick}
             className={clsx(
-                `py-2 px-10 text-center text-2xl font-medium tracking-widest outline outline-2 outline-offset-[-2px] focus:ring-2 focus:ring-current`,
-                !invert && defaultColors.normal,
-                invert && defaultColors.inverted,
+                `py-2 px-10 text-center text-2xl font-medium tracking-widest outline outline-2 outline-offset-[-2px] focus:ring-2 focus:ring-inset focus:ring-offset-2`,
+                !invert &&
+                    "bg-black text-white outline-black hover:bg-white hover:text-black hover:outline-black focus:ring-white dark:bg-white dark:text-black dark:outline-white dark:hover:bg-black dark:hover:text-white dark:hover:outline-black dark:focus:ring-black",
+                invert &&
+                    "bg-white text-black outline-black hover:bg-black hover:text-white focus:ring-black dark:bg-black dark:text-white dark:hover:bg-white dark:hover:text-black dark:focus:ring-white",
                 className,
             )}
         >
